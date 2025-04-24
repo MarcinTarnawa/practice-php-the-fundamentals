4ac5bddaca03cf5a1be5c2d2371ebef3f4eca820
Marcin Tarnawa <marcintarnawa@o2.pl>
2025-04-24 20:27:11 +0200

    zadanie 5

--- 05/index.php
+++ 05/index.php
@@ -3,5 +3,16 @@
 $menu = [
     ['label' => 'home', 'url' => '/'],
     ['label' => 'about me', 'url' => '/about'],
-    ['label' => 'contact', 'url' => 'contact'],
+    ['label' => 'contact', 'url' => '/contact'],
 ];
+
+$items = $menu;
+
+function generateMenu($items) {
+    foreach ($items as $item) {
+        echo $item['label']. " " . $item['url'] . "<br>";
+      }
+}
+
+require "index.view.php";
+
